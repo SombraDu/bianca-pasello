fa8478bc7783fef9656874828299cb0fe802cf4b
SombraDu <dus212@newschool.edu>
2025-12-04 23:11:07 -0500

    1
    1

---
+++ script.js
@@ -0,0 +1,6 @@
+window.onload = () => {
+    document.querySelector('.left-sword').classList.add('animate-left');
+    document.querySelector('.right-sword').classList.add('animate-right');
+    document.querySelector('.title').classList.add('animate-title');
+};
+
